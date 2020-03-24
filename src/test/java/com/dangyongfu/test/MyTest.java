97dab5dacd8bc8b1ecb3b3c3f891475b25fca284
dangyongfu <dangyongfu@qq.com>
2020-03-24 19:55:59 +0800

    day05第二次提交

--- src/test/java/com/dangyongfu/test/MyTest.java
+++ src/test/java/com/dangyongfu/test/MyTest.java
@@ -3,5 +3,6 @@ package com.dangyongfu.test;
 public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("HelloWard");
+		System.out.println("I want to see you any more");
 	}
 }
